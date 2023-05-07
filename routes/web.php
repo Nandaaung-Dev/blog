377e7f4356b2c0dc864aafbbf52b3e267671c79e
Nandaaung-Dev <nandaaung1689@gmail.com>
2023-05-07 09:56:21 +0630

    Routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('articles', function(){
+    return 'Article Lists';
+});
+
+Route::get('articles/detail', function(){
+    return 'Article Detail';
+})->name('articles.detail');
+
+Route::get('articles/detail/{id}', function($id){
+    return "Articles Detail-$id";
+});
+
+Route::get('articles/more', function(){
+    return redirect()->route('articles.detail');
 });
